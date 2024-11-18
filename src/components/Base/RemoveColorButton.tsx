e49c859e7be63b161dc2c3b8b9fff5225865aa6e
marvin19 <marita.vindedal@highsoft.com>
2024-11-18 15:11:48 +0100

    Added focusindicator that changes color based on contrast for remove button.

--- src/components/Base/RemoveColorButton.tsx
+++ src/components/Base/RemoveColorButton.tsx
@@ -1,24 +1,37 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faX } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
 
 interface RemoveColorButtonProps {
     textColor: string;
     removeColorBar: () => void;
 }
 
-const removeColorButton = ({
+const RemoveColorButton = ({
     removeColorBar,
     textColor,
 }: RemoveColorButtonProps): JSX.Element => {
+    const [isFocused, setIsFocused] = useState(false);
+
     return (
         <button
             className="remove-color"
             onClick={removeColorBar}
+            onFocus={() => {
+                setIsFocused(true);
+            }}
+            onBlur={() => {
+                setIsFocused(false);
+            }}
             aria-label="Remove color"
+            style={{
+                outline: isFocused ? `2px solid ${textColor}` : 'none',
+                outlineOffset: '2px',
+            }}
         >
             <FontAwesomeIcon icon={faX} style={{ color: textColor }} />
         </button>
     );
 };
 
-export default removeColorButton;
+export default RemoveColorButton;
